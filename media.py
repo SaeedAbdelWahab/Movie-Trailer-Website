84bc0cb50e2a49304dc8e6bc232eb308ffeb7ad5
Saeed <saeed.abdellwahab@gmail.com>
2018-10-07 12:24:29 +0200

    add the movie class

---
+++ media.py
@@ -0,0 +1,9 @@
+class Movie():
+	"""
+	A class movie which contains some movie attributes (story,trailer,image,title)
+	"""
+	def __init__(self,movie_title,movie_poster_image,movie_youtube_url,movie_story) :
+		self.title = movie_title
+		self.poster_image_url = movie_poster_image
+		self.trailer_youtube_url = movie_youtube_url
+		self.story = movie_story
