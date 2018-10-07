2079c0a6358d50591334502ad0b2e4b28ced539a
Saeed <saeed.abdellwahab@gmail.com>
2018-10-07 12:36:21 +0200

    add the nun movie

---
+++ entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+import fresh_tomatoes
+
+the_nun = media.Movie("The Nun","https://m.media-amazon.com/images/M/MV5BMjEwMDE1NzI3M15BMl5BanBnXkFtZTgwNjg2NjExNjM@._V1_.jpg",\
+	"https://youtu.be/pzD9zGcUNrw","A priest with a haunted past and a novice on the threshold of her final vows are sent by the \
+	Vatican to investigate the death of a young nun in Romania and confront a malevolent force in the form of a demonic nun.")
+
+fresh_tomatoes.open_movies_page([the_nun])
