0b86b793a58d5aff107df5e0e2bb5e16550e883d
Saeed <saeed.abdellwahab@gmail.com>
2018-10-09 08:04:27 +0200

    fix comments

--- media.py
+++ media.py
@@ -1,7 +1,11 @@
 class Movie():
-    """
-    A class movie which contains some movie attributes (story, trailer, image,
-    title)
+    """A class movie which have some movie attributes
+
+    Attributes:
+        title (str): the title of the movie
+        post_image_url (str): The link to the image of the movie
+        trailer_youtube_url (str): The link to the trailer
+        story (str): the story of the movie
     """
     def __init__(self, movie_title, movie_poster_image, movie_youtube_url,
                  movie_story):
